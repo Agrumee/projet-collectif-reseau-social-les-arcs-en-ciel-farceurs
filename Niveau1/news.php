0b565c564df3c2a3ab19d681075071793dd151fa
MehdiGrimault <mehdi.grimault@gmail.com>
2023-10-02 15:01:53 +0200

    finalisation reusinage et etape 1 de la page news

--- Niveau1/news.php
+++ Niveau1/news.php
@@ -6,7 +6,6 @@
 
     <body>
         <?php include("header.php"); ?>
-        <?php include("BDD.php"); ?>
         <div id="wrapper">
             <aside>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
@@ -16,28 +15,7 @@
                         tous les utilisatrices du site.</p>
                 </section>
             </aside>
-            <main>
-                <!-- L'article qui suit est un exemple pour la présentation et 
-                  @todo: doit etre retiré -->
-                <article>
-                    <h3>
-                        <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
-                    </h3>
-                    <address>par AreTirer</address>
-                    <div>
-                        <p>Ceci est un paragraphe</p>
-                        <p>Ceci est un autre paragraphe</p>
-                        <p>... de toutes manières il faut supprimer cet 
-                            article et le remplacer par des informations en 
-                            provenance de la base de donnée (voir ci-dessous)</p>
-                    </div>                                            
-                    <footer>
-                        <small>♥1012 </small>
-                        <a href="">#lorem</a>,
-                        <a href="">#piscitur</a>,
-                    </footer>
-                </article>               
-
+            <main>          
                 <?php
                 /*
                   // C'est ici que le travail PHP commence
@@ -49,7 +27,7 @@
                  */
 
                 // Etape 1: Ouvrir une connexion avec la base de donnée.
-                // $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                include("BDD.php");
                 //verification
                 if ($mysqli->connect_errno)
                 {
@@ -94,7 +72,7 @@
                 {
                     //la ligne ci-dessous doit etre supprimée mais regardez ce 
                     //qu'elle affiche avant pour comprendre comment sont organisées les information dans votre 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    //echo "<pre>" . print_r($post, 1) . "</pre>";
 
                     // @todo : Votre mission c'est de remplacer les AREMPLACER par les bonnes valeurs
                     // ci-dessous par les bonnes valeurs cachées dans la variable $post 
@@ -106,13 +84,13 @@
                         <h3>
                             <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>AREMPLACER</address>
+                        <address><?php echo ("Par " . $post['author_name'] )?></address>
                         <div>
-                            <p>AREMPLACER</p>
+                            <p><?php echo $post['content'] ?></p>
                         </div>
                         <footer>
-                            <small>♥ AREMPLACER </small>
-                            <a href="">AREMPLACER</a>,
+                            <small><?php echo( "♥ " . $post['like_number']) ?></small>
+                            <a href=""><?php echo ( "#" . $post['taglist']) ?></a>,
                         </footer>
                     </article>
                     <?php
