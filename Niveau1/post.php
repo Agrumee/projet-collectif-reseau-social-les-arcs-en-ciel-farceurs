6630cae57e6c8a8d145fab9e442bc142541b99f1
clementine <cleriv.72@gmail.com>
2023-10-05 09:42:40 +0200

    début tache likes

--- Niveau1/post.php
+++ Niveau1/post.php
@@ -1,21 +1,48 @@
-
 <?php
-while ($post = $lesInformations->fetch_assoc())
-                {
-                    ?>                
-                    <article>
-                        <h3>
-                            <time><?php echo $post['created']; ?></time>
-                        </h3>
-                        <address>
-                            <a href="wall.php?user_id=<?php echo $post['author_id']?>"><?php echo ("Par " . $post['author_name']); ?></a>
-                        </address>
-                        <div>
-                            <p><?php echo $post['content']; ?></p>
-                        </div>
-                        <footer>
-                            <small><?php echo( "♥ " . $post['like_number']); ?></small>
-                            <a href=""><?php echo ( "#" . $post['taglist']); ?></a>, 
-                        </footer>
-                    </article>
-                <?php } ?>
+while ($post = $lesInformations->fetch_assoc()) {
+    ?>
+    <article>
+        <h3>
+            <time>
+                <?php echo $post['created']; ?>
+            </time>
+        </h3>
+        <address>
+            <a href="wall.php?user_id=<?php echo $post['author_id'] ?>">
+                <?php echo ("Par " . $post['author_name']); ?>
+            </a>
+        </address>
+        <div>
+            <p>
+                <?php echo $post['content']; ?>
+            </p>
+        </div>
+        <footer>
+            <small>
+
+                <!-- problème à résoudre : la boucle While attribue les likes à tous les posts qui apparaissent sur la page. -->
+                <form method="post" action="<?php $lInstructionSql = "INSERT INTO likes "
+                    . "(id, user_id, post_id) "
+                    . "VALUES (NULL, "
+                    . $userId . ", "
+                    . $post['num_post'] . ");"
+                ;
+                $ok = $mysqli->query($lInstructionSql); ?>">
+                    <input type='hidden' name='???' value='achanger'>
+
+                    <input class="like" type='submit' value="♥">
+                    <?php
+
+                    echo ($post['like_number']); ?>
+
+
+
+
+            </small>
+            </form>
+            <a href="">
+                <?php echo ("#" . $post['taglist']); ?>
+            </a>,
+        </footer>
+    </article>
+<?php } ?>
