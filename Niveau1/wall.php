4b74a59eecc97331e1952003f5493dcf8c195dc4
MehdiGrimault <mehdi.grimault@gmail.com>
2023-10-10 16:51:33 +0200

    modif bug double ou triple likes

--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -76,16 +76,30 @@
                  * Etape 3: récupérer tous les messages de l'utilisatrice
                  */
                 $laQuestionEnSql = "
-                    SELECT posts.content, posts.created, users.alias as author_name, users.id as author_id, posts.id as postId,
-                    COUNT(likes.id) as like_number, GROUP_CONCAT(DISTINCT tags.label) AS taglist 
-                    FROM posts
-                    JOIN users ON  users.id=posts.user_id
-                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
-                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
-                    WHERE posts.user_id='$userId' 
-                    GROUP BY posts.id
-                    ORDER BY posts.created DESC  
+                SELECT posts.content,
+                posts.created,
+                users.alias as author_name,
+                users.id as author_id,
+                posts.id as postId,
+                likes.like_number,
+                tags.taglist
+                FROM posts
+                JOIN users ON users.id = posts.user_id
+                LEFT JOIN (
+                    SELECT post_id, COUNT(DISTINCT id) as like_number
+                    FROM likes
+                    GROUP BY post_id
+                ) as likes ON likes.post_id = posts.id
+                LEFT JOIN (
+                    SELECT post_id, GROUP_CONCAT(DISTINCT tags.label) AS taglist
+                    FROM posts_tags
+                    LEFT JOIN tags ON posts_tags.tag_id = tags.id
+                    GROUP BY post_id
+                ) as tags ON tags.post_id = posts.id
+                WHERE posts.user_id = '$userId'
+                GROUP BY posts.id
+                ORDER BY posts.created DESC;
+                
                     ";
 
                 $lesInformations = $mysqli->query($laQuestionEnSql);
